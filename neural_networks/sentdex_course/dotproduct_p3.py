ecf617c1f25fddc819d7514d318e2576ff0619ed
Luciano <lucianobarrabino@gmail.com>
2023-02-02 15:43:58 -0300

    feature: add dot product p3

---
+++ neural_networks/sentdex_course/dotproduct_p3.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+inputs = [1, 2, 3, 2.5]
+weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
+biases = [ 2, 3, 0.5 ]
+
+output = np.dot(weights, inputs) + biases
+"""
+ the output variable works under the hood: 
+ np.dot([1,2,3], [4,5,6]) = 1*4 + 2*5 + 3*6 = 32
+ np.dot(weights, inputs) = [np.dot(weights[0], inputs), np.dot(weights[1], inputs), np.dot(weights[2], inputs)] = [2.8, -1.79, 1.85]
+ np.dot(weights, inputs) + biases = [2.8, -1.79, 1.85] + [ 2, 3, 0.5 ] = [4.8, 1.21, 2.385]
+"""
+print(output)
--- neural_networks/sentdex_course/layers_p2.py
+++ neural_networks/sentdex_course/layers_p2.py
@@ -1,11 +1,15 @@
 #In this case the input variable is one of the last neurons(output neuron), thats why it takes 4 weights cause before the output neurons there are 4 hidden layers with 3 unique weights each one.
 inputs = [1, 2, 3, 2.5]
 weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
-bias = [ 2, 3, 0.5 ]
+biases = [ 2, 3, 0.5 ]
 
-for x in range(len(bias)):
-    output = 0
+layer_ouputs = []
 
-    output = inputs[0]*weights[x][0] + inputs[1]*weights[x][1] + inputs[2]*weights[x][2] + inputs[3]*weights[x][3] + bias[x]
+for neuron_weights, neuron_bias in zip(weights, biases):
+    neuron_output = 0
+    for n_input, weight in zip(inputs, neuron_weights):
+        neuron_output += n_input*weight
+    neuron_output += neuron_bias
+    layer_ouputs.append(neuron_output)
 
-    print(output)
+print(layer_ouputs)
